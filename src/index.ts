710343c72334e4631e1f2d0764f39cc6784aabb5
amber williams <amber@octopuswealth.com>
2022-09-18 11:37:27 +0100

    Catch unexpected errors then exit app gracefully

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,28 @@ import logger from "./features/logger"
 import "./db/index"
 
 app.on("ready", () => {
-  app.listen(config.port, () => {
+  const server = app.listen(config.port, () => {
     logger.log("info", `Server ready - listening on port:${config.port}`)
   })
+  app.set("server", server)
 })
+
+const exitHandler = () => {
+  const server = app.get("server")
+  if (server) {
+    server.close(() => {
+      logger.log("info", "Server stopped")
+      process.exit(1)
+    })
+  } else {
+    process.exit(1)
+  }
+}
+
+const unexpectedErrorHandler = (error: Error) => {
+  logger.log("error", `Unexpected error ${error}`)
+  exitHandler()
+}
+
+process.on("uncaughtException", unexpectedErrorHandler)
+process.on("unhandledRejection", unexpectedErrorHandler)
